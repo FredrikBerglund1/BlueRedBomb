5d82a52288f2fb9778f44e2d9dc26d078d4ce1e0
Fredrik Berglund <frbe9022@git.dsv.su.se>
2016-06-12 13:00:28 +0200

    first commit

---
+++ BlueRedBomb/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  BlueRedBomb
+//
+//  Created by Fredrik Berglund on 2016-06-09.
+//  Copyright © 2016 Fredrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
+
+    @IBAction func hideRed(sender: AnyObject) {
+        redBomb.hidden = true
+    }
+
+}
+
